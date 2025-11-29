8cb9d576a394dbd1023ba04d5bb9c2851d7722cd
WhaleMilk <collinmbarnett@gmail.com>
2025-11-28 23:28:46 -0700

    added bitflags and registers

---
+++ zig-emu/src/bitflags.zig
@@ -0,0 +1,58 @@
+const std = @import("std");
+
+const flags = enum(u8) {
+    Z = 0b10000000,
+    N = 0b01000000,
+    H = 0b00100000,
+    C = 0b00010000,
+    EMPTY = 0b00000000,
+};
+
+pub fn get_flag_from(val: u8) flags {
+    return switch (val) {
+        0b10000000 => flags.Z,
+        0b01000000 => flags.N,
+        0b00100000 => flags.H,
+        0b00010000 => flags.C,
+        0b00000000 => flags.EMPTY,
+    };
+}
+
+pub const bitflags = struct {
+    B: u8,
+
+    pub fn init() bitflags {
+        return bitflags{
+            .B = @intFromEnum(flags.EMPTY),
+        };
+    }
+
+    pub fn set(self: *bitflags, other: flags, value: bool) void {
+        if (value) {
+            self.B = self.B | @intFromEnum(other);
+        } else {
+            self.B = self.B & ~@intFromEnum(other);
+        }
+    }
+
+    pub fn check(self: bitflags, other: flags) bool {
+        const o = @intFromEnum(other);
+        return self.B & o == o;
+    }
+
+    pub fn bits(self: bitflags) u8 {
+        return self.B;
+    }
+};
+
+test "bitflags_test_1" {
+    var f = bitflags.init();
+    f.set(flags.C, true);
+    try std.testing.expectEqual(0b00010000, f.bits());
+}
+
+test "bitflags_test_2" {
+    var f = bitflags.init();
+    f.set(flags.Z, true);
+    try std.testing.expectEqual(0b10000000, f.bits());
+}
--- zig-emu/src/register_page.zig
+++ zig-emu/src/register_page.zig
@@ -1,5 +1,74 @@
 const std = @import("std");
+const bitflags = @import("bitflags.zig");
 
 const r8 = enum { A, B, C, D, E, F, H, L };
 
 const r16 = enum { AF, BC, DE, HL, SP };
+
+pub const RegisterPage = struct {
+    a: u8,
+    flags: bitflags,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    h: u8,
+    l: u8,
+    sp: u16,
+    pc: u16,
+
+    pub fn init() RegisterPage {
+        return RegisterPage{
+            .a = 0,
+            .flags = bitflags.bitflags.init(),
+            .b = 0,
+            .c = 0,
+            .d = 0,
+            .e = 0,
+            .h = 0,
+            .l = 0,
+            .sp = 0,
+            .pc = 0,
+        };
+    }
+
+    pub fn read16(self: RegisterPage, reg: r16) u16 {
+        return switch (reg) {
+            r16.SP => self.sp,
+            r16.AF => blk: {
+                break :blk ((@as(u16, self.a)) << 8) | self.flags.bitflags.bits();
+            },
+            r16.BC => blk: {
+                break :blk ((@as(u16, self.a)) << 8) | @as(u16, self.c);
+            },
+            r16.DE => blk: {
+                break :blk ((@as(u16, self.d)) << 8) | @as(u16, self.e);
+            },
+            r16.HL => blk: {
+                break :blk ((@as(u16, self.h)) << 8) | @as(u16, self.l);
+            },
+        };
+    }
+
+    pub fn write16(self: *RegisterPage, reg: r16, val: u16) void {
+        switch (reg) {
+            r16.SP => self.sp = val,
+            r16.AF => {
+                self.a = @as(u8, val >> 8);
+                self.f = bitflags.get_flag_from(@truncate(val));
+            },
+            r16.BC => {
+                self.b = @as(u8, val >> 8);
+                self.c = @truncate(val);
+            },
+            r16.DE => {
+                self.d = @as(u8, val >> 8);
+                self.c = @truncate(val);
+            },
+            r16.HL => {
+                self.h = @as(u8, val >> 8);
+                self.l = @truncate(val);
+            },
+        }
+    }
+};
